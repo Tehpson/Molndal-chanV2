4578a264dbadad4e7f5565219c68398bd46740d4
Tehpson <jesper@familjen-pettersson.se>
2021-04-28 16:15:30 +0200

    cahnged name from debug to Console becuse it should be included

---
+++ Mölndal-chan v2/Console/ConsolePrint.cs
@@ -0,0 +1,15 @@
+﻿namespace mölndal_chan_v2.debug
+{
+    using System;
+
+    public static class ConsolePrint
+    {
+        public static void Print(Discord.IGuild guild, string command, Discord.WebSocket.SocketUser user = null)
+        {
+            var time = DateTime.Now.ToString("g");
+            var userstr = user == null ? "Server" : user.Username;
+
+            Console.WriteLine($"{time}  -  {guild.Name}  -  {userstr}  -  {command}");
+        }
+    }
+}
